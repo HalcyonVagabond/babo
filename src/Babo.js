6899f08a0d0f3dbab3e9178dfef65e2b6972acbd
Workspace <Workspace@Praecessor.local>
2021-03-18 08:06:57 -0500

    init commit

---
+++ src/Babo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Babo.css';
+import MainNavigation from './components/layout/MainNavigation'
+import Test from './components/courses/Test'
+
+function Bajo() {
+  return (
+    <div>
+      <MainNavigation>
+        <Test />
+      </MainNavigation>
+    </div>
+  );
+}
+
+export default Bajo;
